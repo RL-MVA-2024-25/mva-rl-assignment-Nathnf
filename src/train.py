f4c3e8275cca0e55a71e5f6fe1f3d9c1d8bae413
Nathan <fredholmnathan77@gmail.com>
2025-01-13 14:37:38 +0100

    train randomization

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 
 env = TimeLimit(
-    env=HIVPatient(domain_randomization=False), max_episode_steps=200
+    env=HIVPatient(domain_randomization=True), max_episode_steps=200
 )  # The time wrapper limits the number of steps in an episode at 200.
 # Now is the floor is yours to implement the agent and train it.
 
